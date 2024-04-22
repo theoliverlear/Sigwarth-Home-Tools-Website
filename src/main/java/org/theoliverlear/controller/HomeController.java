4f9032461b21552c9c67bc9dd19caff974e090d8
theoliverlear <sigwartholiver2@gmail.com>
2024-04-21 20:29:19 -0500

    Add Semaphore to limit concurrent access to home page in HomeController.java

--- src/main/java/org/theoliverlear/controller/HomeController.java
+++ src/main/java/org/theoliverlear/controller/HomeController.java
@@ -3,10 +3,24 @@ package org.theoliverlear.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.Semaphore;
+
 @Controller
 public class HomeController {
+    private final Semaphore SEMAPHORE;
+    public HomeController(Semaphore semaphore) {
+        this.SEMAPHORE = semaphore;
+    }
     @RequestMapping("/")
     public String home() {
-        return "home";
+        if (this.SEMAPHORE.tryAcquire()) {
+            try {
+                return "home";
+            } finally {
+                this.SEMAPHORE.release();
+            }
+        } else {
+            return "too-many-requests";
+        }
     }
 }
