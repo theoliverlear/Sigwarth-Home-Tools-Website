8d5ceb8e9c0a32ec114794ffb36df8d0106c901f
theoliverlear <sigwartholiver2@gmail.com>
2024-04-21 20:43:54 -0500

    Refactor Semaphore to RateLimiter in HomeController.java for improved request throttling

--- src/main/java/org/theoliverlear/controller/HomeController.java
+++ src/main/java/org/theoliverlear/controller/HomeController.java
@@ -1,24 +1,21 @@
 package org.theoliverlear.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class HomeController {
-    private final Semaphore SEMAPHORE;
-    public HomeController(Semaphore semaphore) {
-        this.SEMAPHORE = semaphore;
+    private final RateLimiter RATE_LIMITER;
+    public HomeController(RateLimiter rateLimiter) {
+        this.RATE_LIMITER = rateLimiter;
     }
     @RequestMapping("/")
     public String home() {
-        if (this.SEMAPHORE.tryAcquire()) {
-            try {
-                return "home";
-            } finally {
-                this.SEMAPHORE.release();
-            }
+        boolean isRateLimited = this.RATE_LIMITER.tryAcquire(500, TimeUnit.MILLISECONDS);
+        if (isRateLimited) {
+            return "home";
         } else {
             return "too-many-requests";
         }
