227f9f8fb17cf1fde6165ec46d5d5624c80475d4
theoliverlear <sigwartholiver2@gmail.com>
2024-04-21 20:44:53 -0500

    Add RateLimiter bean to SigwarthHomeToolsApplication.java to control data flow rate

--- src/main/java/org/theoliverlear/SigwarthHomeToolsApplication.java
+++ src/main/java/org/theoliverlear/SigwarthHomeToolsApplication.java
@@ -1,11 +1,17 @@
 package org.theoliverlear;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SigwarthHomeToolsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SigwarthHomeToolsApplication.class, args);
+    }
+    @Bean
+    public RateLimiter rateLimiter() {
+        return RateLimiter.create(1.0);
     }
 }
