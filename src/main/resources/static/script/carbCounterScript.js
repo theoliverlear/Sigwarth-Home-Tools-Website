2fce8efd6be887530ddb97dde834ee2ce6cd1a1f
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-04-26 21:41:10 -0500

    Add carb value calls to the back-end in carbCounterScript.js

--- src/main/resources/static/script/carbCounterScript.js
+++ src/main/resources/static/script/carbCounterScript.js
@@ -1,1 +1,24 @@
-// TODO: Auto-copy to clipboard option
+// TODO: Auto-copy to clipboard option, Image detection
+let carbInputs = document.getElementsByClassName("carb-input");
+let servingSize = document.getElementById("serving-size");
+let unitsMeasured = document.getElementById("units-measured");
+let carbsPerServing = document.getElementById("carbs-per-serving");
+let carbOutputTitleDiv = document.getElementById("carb-output-title-div");
+function updateCarbCount() {
+    fetch("/carbcounter/count", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            unitsPerServing: servingSize.value,
+            unitsMeasured: unitsMeasured.value,
+            carbsPerServing: carbsPerServing.value
+        })
+    }).then(async response => {
+        carbOutputTitleDiv.innerHTML = await response.text();
+    });
+}
+Array.from(carbInputs).forEach(input => {
+    input.addEventListener("input", updateCarbCount);
+});
