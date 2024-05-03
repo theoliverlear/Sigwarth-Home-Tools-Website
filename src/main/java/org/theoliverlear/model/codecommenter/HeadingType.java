f6da44d2c7f59f922b17e8191d2a95ffaa027f55
theoliverlear <sigwartholiver2@gmail.com>
2024-05-02 21:20:30 -0500

    Add method for obtaining HeadingType from name in HeadingType.java

--- src/main/java/org/theoliverlear/model/codecommenter/HeadingType.java
+++ src/main/java/org/theoliverlear/model/codecommenter/HeadingType.java
@@ -15,4 +15,15 @@ public enum HeadingType {
         this.POST_ARTIFACT = POST_ARTIFACT;
         this.SEPARATOR_CHAR = SEPARATOR_CHAR;
     }
+    public static HeadingType fromName(String name) {
+        return switch (name) {
+            case "THIN" -> HeadingType.THIN;
+            case "THICK" -> HeadingType.THICK;
+            case "HTML" -> HeadingType.HTML;
+            case "CSS" -> HeadingType.CSS;
+            case "THIN_HASH" -> HeadingType.THIN_HASH;
+            case "THICK_HASH" -> HeadingType.THICK_HASH;
+            default -> HeadingType.THIN;
+        };
+    }
 }
