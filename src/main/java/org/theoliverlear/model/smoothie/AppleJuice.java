c031aa996f9e73fa16b4a4687726e54d3d06abbf
theoliverlear <sigwartholiver2@gmail.com>
2024-05-06 11:21:32 -0500

    Refactor AppleJuice.java by adding CarbItem, recalculating carbohydrates, and changing expected percentage constant

--- src/main/java/org/theoliverlear/model/smoothie/AppleJuice.java
+++ src/main/java/org/theoliverlear/model/smoothie/AppleJuice.java
@@ -1,20 +1,43 @@
 package org.theoliverlear.model.smoothie;
 //=================================-Imports-==================================
-import org.theoliverlear.model.codecommenter.WeightStatus;
+
+import org.theoliverlear.model.carbcounter.CarbItem;
 
 public class AppleJuice extends SmoothieItem {
+    //============================-Variables-=================================
+    private CarbItem carbItem;
     //============================-Constants-=================================
-    public static final int EXPECTED_PERCENTAGE = 44;
+    public static final int EXPECTED_PERCENTAGE = 34;
+    public static final int SERVING_WEIGHT = 240;
+    public static final int SERVING_CARBS = 16;
     //===========================-Constructors-===============================
     public AppleJuice(int weight) {
         super("Apple Juice", weight);
+        this.carbItem = new CarbItem(SERVING_WEIGHT, this.getWeight(), SERVING_CARBS);
     }
     //============================-Overrides-=================================
 
     //-------------------------Get-Weight-Status------------------------------
     @Override
     public WeightStatus getWeightStatus(int currentPercentage) {
-        double ratio = (double) currentPercentage / (double) EXPECTED_PERCENTAGE;
+        double ratio = this.getWeightRatio(currentPercentage);
         return WeightStatus.fromRatio(ratio);
+    }
+    //-------------------------Get-Weight-Ratio-------------------------------
+    @Override
+    public double getWeightRatio(int currentPercentage) {
+        return (double) currentPercentage / (double) EXPECTED_PERCENTAGE;
+    }
+    //-----------------------------Get-Carbs----------------------------------
+    @Override
+    public int getCarbs() {
+        this.carbItem.calculateCarbs();
+        return this.carbItem.getCarbs();
+    }
+    //-----------------------------Set-Weight---------------------------------
+    @Override
+    public void setWeight(int weight) {
+        super.setWeight(weight);
+        this.carbItem.setWeightUnits(this.getWeight());
     }
 }
