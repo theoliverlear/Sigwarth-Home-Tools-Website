6377f57be5072d35dfe1132f4a170f8a4eaa29a8
theoliverlear <sigwartholiver2@gmail.com>
2024-05-06 11:28:26 -0500

    Add SmoothieResponse.java to handle smoothie related responses

---
+++ src/main/java/org/theoliverlear/communication/SmoothieResponse.java
@@ -0,0 +1,37 @@
+package org.theoliverlear.communication;
+
+import lombok.Data;
+import org.theoliverlear.model.smoothie.Smoothie;
+
+@Data
+public class SmoothieResponse {
+    String smoothieStatus;
+    String appleJuiceStatus;
+    String yogurtStatus;
+    String frozenFruitStatus;
+    int appleJuicePercentage;
+    int yogurtPercentage;
+    int frozenFruitPercentage;
+    int carbsPerDrink;
+    public SmoothieResponse(String smoothieStatus, String appleJuiceStatus, String yogurtStatus, String frozenFruitStatus,
+                            int appleJuicePercentage, int yogurtPercentage, int frozenFruitPercentage, int carbsPerDrink) {
+        this.smoothieStatus = smoothieStatus;
+        this.appleJuiceStatus = appleJuiceStatus;
+        this.yogurtStatus = yogurtStatus;
+        this.frozenFruitStatus = frozenFruitStatus;
+        this.appleJuicePercentage = appleJuicePercentage;
+        this.yogurtPercentage = yogurtPercentage;
+        this.frozenFruitPercentage = frozenFruitPercentage;
+        this.carbsPerDrink = carbsPerDrink;
+    }
+    public SmoothieResponse(Smoothie smoothie) {
+        this.smoothieStatus = smoothie.getSmoothieStatus().SMOOTHIE_STATUS;
+        this.appleJuiceStatus = smoothie.getAppleJuiceWeightStatus().WEIGHT_STATUS;
+        this.yogurtStatus = smoothie.getYogurtWeightStatus().WEIGHT_STATUS;
+        this.frozenFruitStatus = smoothie.getFrozenFruitWeightStatus().WEIGHT_STATUS;
+        this.appleJuicePercentage = smoothie.getAppleJuicePercentage();
+        this.yogurtPercentage = smoothie.getYogurtPercentage();
+        this.frozenFruitPercentage = smoothie.getFrozenFruitPercentage();
+        this.carbsPerDrink = smoothie.getCarbsPerDrink();
+    }
+}
