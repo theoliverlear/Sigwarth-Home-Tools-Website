caff14696aa940064d28a8e11575dc907b7ca322
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 16:39:08 -0600

    Refactor RATE_LIMITER to rateLimiter for variable naming consistency in HomeController.java

--- src/main/java/org/theoliverlear/controller/HomeController.java
+++ src/main/java/org/theoliverlear/controller/HomeController.java
@@ -8,14 +8,14 @@ import java.util.concurrent.TimeUnit;
 
 @Controller
 public class HomeController {
-    private final RateLimiter RATE_LIMITER;
+    private final RateLimiter rateLimiter;
     @Autowired
     public HomeController(RateLimiter rateLimiter) {
-        this.RATE_LIMITER = rateLimiter;
+        this.rateLimiter = rateLimiter;
     }
     @RequestMapping("/")
     public String home() {
-        boolean isRateLimited = this.RATE_LIMITER.tryAcquire(500, TimeUnit.MILLISECONDS);
+        boolean isRateLimited = this.rateLimiter.tryAcquire(500, TimeUnit.MILLISECONDS);
         if (isRateLimited) {
             return "home";
         } else {
