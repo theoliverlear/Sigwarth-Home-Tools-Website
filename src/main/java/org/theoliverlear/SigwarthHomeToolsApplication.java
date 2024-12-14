427a1a798566d43bd775c78fc34d13462eaa5fbe
theoliverlear <sigwartholiver2@gmail.com>
2024-12-13 18:04:43 -0600

    Exclude DataSourceAutoConfiguration in SigwarthHomeToolsApplication.java to prevent unwanted datasource configuration

--- src/main/java/org/theoliverlear/SigwarthHomeToolsApplication.java
+++ src/main/java/org/theoliverlear/SigwarthHomeToolsApplication.java
@@ -3,9 +3,10 @@ package org.theoliverlear;
 import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SigwarthHomeToolsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SigwarthHomeToolsApplication.class, args);
