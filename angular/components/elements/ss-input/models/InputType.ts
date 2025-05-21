924a7c388978e3d9eeeea6545623e916c0249138
theoliverlear <sigwartholiver2@gmail.com>
2025-05-21 17:16:45 -0500

    Add InputType.ts to define HTML input types used in the Angular application

---
+++ angular/components/elements/ss-input/models/InputType.ts
@@ -0,0 +1,10 @@
+export enum InputType {
+    NUMBER = 'number',
+    TEXT = 'text',
+    PASSWORD = 'password',
+    EMAIL = 'email',
+    TELEPHONE_PAD = 'tel',
+    CHECKBOX = 'checkbox',
+    RANGE = 'range',
+    DATE = 'date',
+}
