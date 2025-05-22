ba8689ee5b48cf9ed3e1447f003fc169cc09f9f8
theoliverlear <sigwartholiver2@gmail.com>
2025-05-21 19:31:35 -0500

    Move SmoothieRequest.java to the `request` package to better organize the package structure

--- src/main/java/org/theoliverlear/communication/request/SmoothieRequest.java
+++ src/main/java/org/theoliverlear/communication/request/SmoothieRequest.java
@@ -1,13 +1,20 @@
-package org.theoliverlear.communication;
+package org.theoliverlear.communication.request;
 
 import lombok.Data;
 
 @Data
 public class SmoothieRequest {
-    int appleJuiceWeight;
-    int yogurtWeight;
-    int frozenFruitWeight;
-    int numDrinks;
+    private int appleJuiceWeight;
+    private int yogurtWeight;
+    private int frozenFruitWeight;
+    private int numDrinks;
+
+    public SmoothieRequest() {
+        this.appleJuiceWeight = 0;
+        this.yogurtWeight = 0;
+        this.frozenFruitWeight = 0;
+        this.numDrinks = 2;
+    }
     public SmoothieRequest(int appleJuiceWeight, int yogurtWeight, int frozenFruitWeight, int numDrinks) {
         this.appleJuiceWeight = appleJuiceWeight;
         this.yogurtWeight = yogurtWeight;
