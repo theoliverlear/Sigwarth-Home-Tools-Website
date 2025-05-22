b9b500efbe22775b3f001a8739e2e2916061ebb7
theoliverlear <sigwartholiver2@gmail.com>
2025-05-21 19:44:11 -0500

    Refactor curly brace to match Java conventions

--- src/main/java/org/theoliverlear/config/WebConfig.java
+++ src/main/java/org/theoliverlear/config/WebConfig.java
@@ -5,8 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer
-{
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
