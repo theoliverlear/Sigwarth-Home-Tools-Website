e40ae9c75928df0527046dd336322a4ebb7497b2
theoliverlear <sigwartholiver2@gmail.com>
2025-05-21 19:47:43 -0500

    Remove HTML-serving method from CodeCommenterController.java as the back-end no longer serves webpages

--- src/main/java/org/theoliverlear/controller/codecommenter/CodeCommenterController.java
+++ src/main/java/org/theoliverlear/controller/codecommenter/CodeCommenterController.java
@@ -1,33 +1,29 @@
 package org.theoliverlear.controller.codecommenter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.theoliverlear.communication.CodeCommentRequest;
+import org.springframework.web.bind.annotation.RestController;
+import org.theoliverlear.communication.request.CodeCommentRequest;
 import org.theoliverlear.model.codecommenter.CommentBuilder;
 import org.theoliverlear.model.codecommenter.HeadingType;
 
-
-@RequestMapping("/code-commenter")
-@Controller
+@RequestMapping("/api/code-commenter")
+@RestController
 public class CodeCommenterController {
-    CommentBuilder commentBuilder = new CommentBuilder();
-    @RequestMapping("/")
-    public String index() {
-        return "code-commenter";
+    CommentBuilder commentBuilder;
+    @Autowired
+    public CodeCommenterController(CommentBuilder commentBuilder) {
+        this.commentBuilder = commentBuilder;
     }
+
     @RequestMapping("/generate")
     public ResponseEntity<String> generate(@RequestBody CodeCommentRequest codeCommentRequest) {
         HeadingType headingType = HeadingType.fromName(codeCommentRequest.getType());
         String textContent = codeCommentRequest.getTextContent().trim();
         int indentLevel = codeCommentRequest.getIndentLevel();
-        if (indentLevel > 5) {
-            indentLevel = 5;
-        } else if (indentLevel < 0) {
-            indentLevel = 0;
-        }
         this.commentBuilder.buildComment(headingType, textContent, indentLevel);
         return new ResponseEntity<>(this.commentBuilder.getComment(), HttpStatus.OK);
     }
