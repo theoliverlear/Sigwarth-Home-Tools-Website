0e17484d8332beb7cbde64f6f99ac13089b80623
theoliverlear <sigwartholiver2@gmail.com>
2025-05-21 19:48:19 -0500

    Remove HTML-serving method from CarbCounterController.java as the back-end no longer serves webpages

--- src/main/java/org/theoliverlear/controller/carbcounter/CarbCounterController.java
+++ src/main/java/org/theoliverlear/controller/carbcounter/CarbCounterController.java
@@ -2,21 +2,16 @@ package org.theoliverlear.controller.carbcounter;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.theoliverlear.communication.CarbItemRequest;
-import org.theoliverlear.communication.TotalCarbResponse;
+import org.springframework.web.bind.annotation.RestController;
+import org.theoliverlear.communication.request.CarbItemRequest;
+import org.theoliverlear.communication.response.TotalCarbResponse;
 import org.theoliverlear.model.carbcounter.CarbItem;
 
-@Controller
-@RequestMapping("/carb-counter")
+@RestController
+@RequestMapping("/api/carb-counter")
 public class CarbCounterController {
-    @RequestMapping("/")
-    public String carbCounter(Model model) {
-        return "carb-counter";
-    }
     @RequestMapping("/count")
     public synchronized ResponseEntity<TotalCarbResponse> count(@RequestBody CarbItemRequest request) {
         CarbItem carbItem = new CarbItem(request.getServingWeight(), request.getUnitsMeasured(), request.getCarbsPerServing());
